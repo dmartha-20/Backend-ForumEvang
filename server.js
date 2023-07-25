9e03309746ccc86ac20e7917d09f14b5e80e7095
dmartha-20 <131492091+dmartha-20@users.noreply.github.com>
2023-07-24 20:15:22 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+require('dotenv').config()
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 80;
+const userRouter = require('./server/api/users/user.router')
+const questionRouter = require('./server/api/question/question.router');
+const answerRouter = require('./server/api/answer/answer.router');
+
+app.use(cors());
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use('/api/users', userRouter);
+app.use("/api/questions", questionRouter);
+app.use("/api/answer",answerRouter);
+
+
+
+app.listen( port ,"0.0.0.0", () => {
+    console.log(`server running at http://localhost:${port}`);
+})
+
+
+
